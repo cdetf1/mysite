caf54b429e33693e6e234a318e9a8b463bbab4ab
cdetf1 <cdetf1@gmail.com>
2024-06-02 00:22:42 +0900

    한번에 업뎃

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'trydb',
+        'USER' : 'root',
+        'PASSWORD' : '10042636', # 설정한 비밀번호로 적어주면 된다.
+        'HOST' : '127.0.0.1',
+        'PORT' : '3306',
     }
 }
 
--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,26 @@ from django.urls import path
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
-    path("", views.index, name="index"),
+    # # ex: /polls/
+    # path('', views.index, name='index'),
+    # # ex: /polls/5/
+    # path('<int:question_id>/', views.detail, name='detail'),
+    # # ex: /polls/5/results/
+    # path('<int:question_id>/results/', views.results, name='results'),
+    # # ex: /polls/5/vote/
+    # path('<int:question_id>/vote/', views.vote, name='vote'),
+    # # # the 'name' value as called by the {% url %} template tag
+    # # path('<int:question_id>/', views.detail, name='detail'),
+    # # added the word 'specifics'
+    # path('specifics/<int:question_id>/', views.detail, name='detail'),
+    
+
+    # 두 번째 및 세 번째 패턴의 경로 문자열에서 일치하는 패턴의 이름이 에서 로 변경 <question_id>되었습니다 <pk>.
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+
 ]
